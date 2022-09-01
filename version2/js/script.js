78ff77541563a19be5abe087dff071836d588646
giomen88 <giorgia9288@virgilio.it>
2022-09-01 19:41:08 +0200

    modifica cdn vue

--- version2/js/script.js
+++ version2/js/script.js
@@ -1,3 +1,7 @@
+console.log('ok', Vue);
+
+Vue.config.devtools = true;
+
 const root = new Vue({
     el: '#root',
     data: {
@@ -6,7 +10,7 @@ const root = new Vue({
     methods:{
         mounted() {
             axios.get("http://localhost/php-ajax-dischi/api/discs").then((res) => {
-                this.discs.push(res.data);
+                this.discs.push(res.data)
             })
         }
     }
