8ddb622dc8919ff6129befb690795a25ccaac55a
giomen88 <giorgia9288@virgilio.it>
2022-09-02 10:09:15 +0200

    correzione script

--- version2/js/script.js
+++ version2/js/script.js
@@ -1,17 +1,13 @@
-console.log('ok', Vue);
-
 Vue.config.devtools = true;
 
-const root = new Vue({
+const app = new Vue({
     el: '#root',
     data: {
         discs: []
     },
-    methods:{
-        mounted() {
-            axios.get("http://localhost/php-ajax-dischi/api/discs").then((res) => {
-                this.discs.push(res.data)
+    mounted() {
+            axios.get("http://localhost/php-ajax-dischi/api/discs").then(res => {
+                this.discs = res.data;
             })
-        }
-    }
+    },
 })
